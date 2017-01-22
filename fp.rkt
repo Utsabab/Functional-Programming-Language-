62dfff3af6f9d1a951a23d5a81a20e6f79d08002
Utsab Khakurel <Utsab Khakurel>
2017-01-21 19:00:34 -0500

    reverse the list but doesnot reverse the sublist

--- fp.rkt
+++ fp.rkt
@@ -1,6 +1,9 @@
 (DEFINE (reverse-general L)
-       
         (if (NULL? L)  ;if the list is empty, return empty list 
-            L 
-        )
+            L
+        
+        (append (reverse-general (CDR L))(list (CAR L)))
+
+
+            )
 )
