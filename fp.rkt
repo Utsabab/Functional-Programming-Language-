f61b9229d990b0846edd3b331e86db0a34312322
Utsab Khakurel <Utsab Khakurel>
2017-01-23 17:07:05 -0500

    QUestion 2: Returns 0 if the list is empty

--- fp.rkt
+++ fp.rkt
@@ -1,10 +1,25 @@
 (DEFINE (reverse-general L)
-        (cond ((NULL? L)  ;if the list is empty, return empty list 
+        (cond
+          ((NULL? L)  ;if the list is empty, return empty list 
             L)
-        ((list? (CAR L))
+          ((list? (CAR L))
             (append (reverse-general (CDR L)) (list (reverse-general (CAR L))))) ;reverses the sublist 
-            (else (append (reverse-general (CDR L)) (list (CAR L))))) ;converts each atom into list and appends them all  
+            (else (append (reverse-general (CDR L)) (list (CAR L)))) ;converts each atom into list and appends them all  
            
-)
+))
+
+(DEFINE (sum-up-numbers-simple L)
+        (cond ((NULL? L) 
+               0)
+              
+))
+              
+               
+               
 
 
+
+
+
+               
+              
