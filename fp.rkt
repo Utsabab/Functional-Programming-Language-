a01e20a9e91fe74b35e78d119fe618eb4ee08bee
Utsab Khakurel <Utsab Khakurel>
2017-01-24 13:30:27 -0500

    Question 3: Same code from Question 2 to work on

--- fp.rkt
+++ fp.rkt
@@ -16,8 +16,16 @@
                (else
                (sum-up-numbers-simple(CDR L));if the first element is not numeric, then recursion to the CDR of list 
 )))
-               
-               
+
+(DEFINE (sum-up-numbers-general L)
+        (cond ((NULL? L) ;if the list is empty, returns the list
+               0)
+              ((number? (CAR L));if the first element of list is number goes to the next line
+               (+ (CAR L) (sum-up-numbers-general (CDR L)))) ;adds up all the numeric atoms in the list
+               (else
+               (sum-up-numbers-general(CDR L));if the first element is not numeric, then recursion to the CDR of list 
+)))
+
 
 
 
