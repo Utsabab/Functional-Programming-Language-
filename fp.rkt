6696cbca3a2fc8f33048731314b302f81714d935
Utsab Khakurel <Utsab Khakurel>
2017-01-24 20:03:45 -0500

    sums all the numeric value even from the sublist

--- fp.rkt
+++ fp.rkt
@@ -21,14 +21,13 @@
         (cond ((NULL? L) ;if the list is empty, returns the list
                0)
               ((list? (CAR L)) 
-               (+(sum-up-numbers-general (CAR L)))) ;if the CAR L is list then recursion 
+               (+(sum-up-numbers-general (CAR L)) (sum-up-numbers-general (CDR L)))) ;if the CAR L is list then recursion 
               
               ((number? (CAR L));if the first element of list is number goes to the next line
                (+ (CAR L) (sum-up-numbers-general (CDR L)))) ;adds up all the numeric atoms in the list
                (else
                (sum-up-numbers-general(CDR L));if the first element is not numeric, then recursion to the CDR of list 
 )))
-
 
 
 
